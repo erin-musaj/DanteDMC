c4b84e0029c420765f5c9998d45152043e2023d5
erin-musaj <erin.musaj@buonarroti.tn.it>
2025-01-29 14:08:47 +0100

    final commit

---
+++ non lo so/src/MulticastSender.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+public class MulticastSender {
+    public static void main(String[] args) {
+        String multicastAddress = "230.0.0.1";
+        int port = 4446;
+        String fileName = "text";
+
+        try (MulticastSocket socket = new MulticastSocket()) {
+            InetAddress group = InetAddress.getByName(multicastAddress);
+            String message="";
+            BufferedReader reader = new BufferedReader(new FileReader("W:\\5INA\\TPSIT\\DanteDMC\\non lo so\\src\\" + fileName));
+
+            while ((message = reader.readLine()) != null) {
+                byte[] buffer = message.getBytes();
+
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length, group, port);
+                socket.send(packet);
+
+                System.out.println("Inviato: " + message);
+
+                Thread.sleep(1000);
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
